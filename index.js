ec01cc48d2c46ce48f5f3a04e12f0bc2b92cd9bf
Allex Welter <allexwelter13@gmail.com>
2025-02-12 08:13:42 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const express = require('express')
+import express from 'express'
+const app = express()
+import mysql from 'mysql2'
+
+
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'allex123',
+    database:'Projeto_Final_Harve_Form'
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err)
+        return
+    }
+    console.log('Conectado ao banco de dados MysQL')
+})
+
+
+app.use(express.json())
