3a3c3f542d18fdbbe893786caebd0bc0836484c8
Allex Welter <allexwelter13@gmail.com>
2025-02-14 06:26:15 -0300

    .

--- routes/testes.js
+++ routes/testes.js
@@ -1,1 +1,14 @@
 // Componente apenas para testes e rascunhos do projeto.
+
+// Endpoints:
+
+// /users (GET): Lista todos os usuários (opcional, pode ser removido se não for necessário).
+
+// /iniciar (POST): Inicia um novo quiz e cria um usuário, se necessário. --> OK
+
+// /perguntas/:numero (GET): Retorna uma pergunta e suas alternativas, além do total de perguntas.
+
+// /enviar (POST): Recebe as respostas do quiz, valida, salva no banco de dados e calcula a pontuação.
+
+// /email/:idQuiz (GET): Envia um email com o resultado do quiz.
+
